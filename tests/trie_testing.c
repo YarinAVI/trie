84577317ed342589e9736ddb06f0f55199e3e07a
Yarin Avi <yarinavisidris100@gmail.com>
2023-05-24 16:30:57 +0300

    new

---
+++ tests/trie_testing.c
@@ -0,0 +1,39 @@
+#include "../trie.h"
+#include <stdio.h>
+
+int main() {
+    int i;
+    Trie t;
+    const char *f;
+    const char *strings[4] = {
+        "yarin",
+        "hello",
+        "hellow",
+        "hellno"
+    };
+    t = trie();
+    for(i=0;i<4;i++) {
+        trie_insert(t, strings[i], (char*)strings[i]);
+    }
+    for(i=0;i<4;i++) {
+        f = trie_exists(t, strings[i]);
+        if(f != NULL) {
+            printf("%s\n",f);
+        }
+        else {
+            printf("string:'%s' could not be found.\n",strings[i]);
+        }
+    }
+    trie_delete(t, strings[1]);
+    for(i=0;i<4;i++) {
+        f = trie_exists(t, strings[i]);
+        if(f != NULL) {
+            printf("%s\n",f);
+        }
+        else {
+            printf("string:'%s' could not be found.\n",strings[i]);
+        }
+    }
+    trie_destroy(&t);
+    return 0;
+}
